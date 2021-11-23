dce07a7ba04f4cacdd883ae86547ddae07cfb252
gustavomes <gustavomes@yahoo.com.br>
2021-11-23 15:03:11 -0300

    segundo commit

---
+++ Checkpoint II/Checkpoint02-FrontendII/script.js
@@ -0,0 +1,7 @@
+/* A primeira página deve ter um formulário com os inputs: 
+Data de criação: o usuário não poderá alterar esse input, mas ele deve ser exibido.
+Data limite da tarefa: data que o usuário deseja terminar aquela tarefa.
+Descrição: texto da tarefa.
+Botão de submit.
+ */
+
